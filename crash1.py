5e10bd289df38efa15ea6c580e313a2fa865c956
Graham913 <gsteven40@charlottesvilleschools.org>
2022-09-16 14:11:44 -0400

    fved

---
+++ crash1.py
@@ -0,0 +1,28 @@
+#type:ignore
+import time
+import board
+import adafruit_mpu605
+import busio
+
+sda_pin = board.GP14
+scl_pin = board.GP15
+i2c = busio.I2C(GP15, GP14)
+mpu = adafruit_mpu6050.MPU6050(i2c)
+
+while True:
+    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
+    print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
+    print("Temperature: %.2f C" % mpu.temperature)
+    print("")
+    time.sleep(1)
+
+
+
+
+
+
+
+
+
+
+
