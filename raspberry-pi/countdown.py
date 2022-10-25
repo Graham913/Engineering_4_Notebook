318d37cbcf52823bcd7b57cf3c713cc8d0cba6a9
Graham913 <gsteven40@charlottesvilleschools.org>
2022-10-25 13:23:53 -0400

    grg

--- raspberry-pi/countdown.py
+++ raspberry-pi/countdown.py
@@ -3,14 +3,14 @@ import digitalio
 import board
 import time
 
-led1 = digitalio.DigitalInOut(board.GP13)
+led1 = digitalio.DigitalInOut(board.GP13) # where the leds are connected to the pico
 led2 = digitalio.DigitalInOut(board.GP18)
 led1.direction = digitalio.Direction.OUTPUT
 led2.direction = digitalio.Direction.OUTPUT
 for x in range(10, -1,-1):
   if x == 0:
-    print("liftoff!")
-    led1.value = True
+    print("liftoff!") 
+    led1.value = True # check true or false and make sure they are capitalized
     time.sleep(.2)
     led1.value = False
     time.sleep(.2)
--- raspberry-pi/countdown1.py
+++ raspberry-pi/countdown1.py
@@ -4,9 +4,9 @@ import board
 import time
 
 for x in range(10, -1,-1):
-  if x == 0:
+  if x == 0: #remember to use two equal signs in this instancce
     print("liftoff!")
 
   else:
     print(x)
-    time.sleep(1)
+    time.sleep(1) # lower the number the faster it will count down
